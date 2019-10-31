a13ed25768bfafad29bd8944e08a6e94e8917f6c
Lars Iversen <lars_iversen2545@hotmail.com>
2019-10-31 22:36:18 +0100

    Den færdige side

--- grums/wp-content/themes/oceanwp-child/functions.php
+++ grums/wp-content/themes/oceanwp-child/functions.php
@@ -10,4 +10,15 @@ function my_theme_enqueue_styles() {
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
+  
 }
+
+/*
+add_action( 'wp_enqueue_scripts', 'pictureChange' );
+function pictureChange() {
+    wp_deregister_script('jquery');
+    wp_enqueue_script('jquery', "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js", array(), null, true);
+    wp_enqueue_script( 'picturechange-js', get_stylesheet_directory_uri() . '/js/picturechange.js', array( 'jquery' ), null, true );
+}
+*/
+
---
+++ grums/wp-content/themes/oceanwp-child/js/picturechange.js
@@ -0,0 +1,9 @@
+/*jQuery(function($){
+
+        $( "#havknap" ).click(function() {
+            $(".elementor-image, img").attr('src','wp-content/uploads/2019/10/girl.jpg');
+
+            });
+        });
+*/
+
